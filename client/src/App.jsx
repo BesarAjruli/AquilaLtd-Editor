29c8b270d4ec8f8ff5cfdcb263a306ab22d3a84e
BesarAjruli <aquila1912@gmail.com>
2025-01-18 16:27:32 +0100

    History (undo/redo) feature

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -154,16 +154,15 @@ export default function App() {
             style: formattedStyle,
             component: React.cloneElement(currentElement.component, { style: formattedStyle }),
           };
-          if(!chngStyle.changing){
-        setElements((prev) => [...prev, updatedElement]);
-        } else {
-          setElements((prev) =>
-            prev.map((element) =>
-              element.id === currentElement.id ? updatedElement : element
+          const newElements = chngStyle.changing
+          ? elements.map((el) =>
+              el.id === currentElement.id ? updatedElement : el
             )
-          );
-          setChangingStyle(false)
-        }
+          : [...elements, updatedElement];
+  
+        setElements(newElements);
+        saveHistory(newElements);
+        setChangingStyle(false);
         setCurrentElement(null);
         }
     e.target.reset()
@@ -197,8 +196,8 @@ export default function App() {
   }
 
   const closeDialog = () => {
-    dialogRef.current.close()
     dialogRef.current.querySelector('form').reset()
+    dialogRef.current.close()
   }
 
   const setWidthMax = (e) => {
@@ -228,18 +227,20 @@ export default function App() {
 }
 
   const deleteElement = () => {
-    setElements((prev) => prev.filter(element => element.id !== chngStyle.id))
-    closeDialog()
+    const newElements = elements.filter((el) => el.id !== chngStyle.id);
+    setElements(newElements);
+    saveHistory(newElements);
+    dialogRef.current.close();
   }
 
   return (
     <>
       <div className='toolBar'>
         <div className='history'>
-        <button disabled={historyIndex === 0}>
+        <button onClick={undoFunction} disabled={historyIndex === 0}>
           <svg  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>undo</title><path d="M12.5,8C9.85,8 7.45,9 5.6,10.6L2,7V16H11L7.38,12.38C8.77,11.22 10.54,10.5 12.5,10.5C16.04,10.5 19.05,12.81 20.1,16L22.47,15.22C21.08,11.03 17.15,8 12.5,8Z" /></svg>
         </button>
-        <button disabled={historyIndex === history.length - 1}>
+        <button onClick={redoFunction} disabled={historyIndex === history.length - 1}>
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>redo</title><path d="M18.4,10.6C16.55,9 14.15,8 11.5,8C6.85,8 2.92,11.03 1.54,15.22L3.9,16C4.95,12.81 7.95,10.5 11.5,10.5C13.45,10.5 15.23,11.22 16.62,12.38L13,16H22V7L18.4,10.6Z" /></svg>
         </button>
         </div>
