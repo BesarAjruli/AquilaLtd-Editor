721b848d2fd811576675593b6f24b60ada800019
BesarAjruli <aquila1912@gmail.com>
2025-02-24 19:12:32 +0100

    Fixed backend new cors url

--- server/app.js
+++ server/app.js
@@ -30,7 +30,7 @@ cloudinary.config({
 });
 
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://aquila-editor.up.railway.app'],
+    origin: ['http://localhost:5173', 'https://aquila-editor.web.app'],
     methods: 'GET,POST,PUT,DELETE',
     allowedHeaders: 'Content-Type,Authorization',
     credentials: true,
