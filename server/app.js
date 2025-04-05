14e2c4d2fc15f661701286f92ab4e8e18099e83f
BesarAjruli <aquila1912@gmail.com>
2025-04-06 01:24:02 +0200

    Changed domain to aploseditor

--- server/app.js
+++ server/app.js
@@ -32,7 +32,7 @@ cloudinary.config({
 });
 
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://aquila-editor.web.app'],
+    origin: ['http://localhost:5173', 'https://aploseditor.web.app'],
     methods: 'GET,POST,PUT,DELETE',
     allowedHeaders: 'Content-Type,Authorization',
     credentials: true,
